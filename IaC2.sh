12acd6c61654afa5d38f932089d84fe1e70d907a
matheusmalt <matheusmalta.pessoal@gmail.com>
2022-08-06 22:09:24 -0300

    Servidor Apache Automatico

---
+++ IaC2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Atualizar
+echo "Atualizando servidor"
+apt update
+apt upgrade -y
+
+
+# Preparando ambiente
+echo "Preparando ambiente"
+apt install apache2 -y
+apt install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
